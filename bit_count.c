6d7e50ab9ff0c03a2727cb490907e27a1c0ea6ae
Yadhunandana Kumaraiah <yadhu.bmsce@gmail.com>
2024-02-03 20:08:18 +0100

    Added another approach to count number of bits in an integer

--- bit_count.c
+++ bit_count.c
@@ -9,8 +9,24 @@ uint8_t bitCount(uint64_t data)
     return (lastTwoBits ? (lastTwoBits == 3 ? 2 : 1) : 1) + bitCount(data >> 2);
 }
 
+uint8_t countBits(uint64_t data)
+{
+    uint8_t bitCounter = 0;
+    while (data > 0)
+    {
+        uint8_t lastTwoBits = data & 0x3;
+        bitCounter += (lastTwoBits) > 0 ? (lastTwoBits == 3 ? 2 : 1) : 0;
+        data = data >> 2;
+    }
+    return bitCounter;
+}
+
 int main()
 {
-    printf("bitcount(%d): ", bitCount(0xFFFF));
+    printf("bitcount(%d)\n", bitCount(0xFFFF));
+    printf("countBits(%d)\n", countBits(0xFFFF));
+
+    printf("bitcount(%d)\n", bitCount(0x9));
+    printf("countBits(%d)\n", countBits(0x9));
     return 0;
 }
